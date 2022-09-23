20c401b2c4bd7150ec06d21bea50d1e32952f299
David C. Page <davidpage876@gmail.com>
2022-09-23 11:50:41 +1000

    Add week 2 exercises

---
+++ week2_exercises.py
@@ -0,0 +1,19 @@
+"""Exercises for week 2 lecture"""
+
+
+# def print_grid(number_of_rows, number_of_columns):
+#     for i in range(number_of_rows):
+#         print("*" * number_of_columns)
+#
+#
+# print_grid(3, 7)
+
+
+# def get_limits():
+#     low = int(input("Low: "))
+#     high = int(input("High: "))
+#     return low, high
+#
+#
+# x, _ = get_limits()
+# print(x)
