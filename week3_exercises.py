6cf328de3a633b590ef0f626d2c8ec9e347dc1b9
David C. Page <davidpage876@gmail.com>
2022-09-28 13:32:34 +1000

    Add csv file read exercise

--- week3_exercises.py
+++ week3_exercises.py
@@ -1,13 +1,27 @@
 """Exercises for week 3 lecture"""
-FILENAME = "week2_exercises.py"
+# FILENAME = "week2_exercises.py"
+#
+#
+# def main():
+#     infile = open(FILENAME)
+#     for line in infile:
+#         if line[0] == "#":
+#             print(line.strip())
+#     infile.close()
+#
+#
+# main()
 
 
-def main():
-    infile = open(FILENAME)
-    for line in infile:
-        if line[0] == "#":
-            print(line.strip())
-    infile.close()
+with open("data_test.csv", "r") as in_file:
+    in_file.readline()  # Ignore header.
+    for line in in_file:
 
+        parts = line.strip().split(',')
+        building = parts[0]
+        cost = float(parts[1])
 
-main()
+        # building, cost = line.strip().split(',')
+        # cost = float(cost)
+
+        print(f"{building} costs ${cost:.2f}")
