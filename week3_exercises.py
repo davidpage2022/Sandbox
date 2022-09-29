5bec487d3c0232810f769fd805ef0c00531cec00
David C. Page <davidpage876@gmail.com>
2022-09-29 11:57:05 +1000

    Add teacher example to find the bug as requested

--- week3_exercises.py
+++ week3_exercises.py
@@ -1,4 +1,5 @@
 """Exercises for week 3 lecture"""
+
 # FILENAME = "week2_exercises.py"
 #
 #
@@ -13,15 +14,55 @@
 # main()
 
 
-with open("data_test.csv", "r") as in_file:
-    in_file.readline()  # Ignore header.
-    for line in in_file:
+# with open("data_test.csv", "r") as in_file:
+#     in_file.readline()  # Ignore header.
+#     for line in in_file:
+#
+#         parts = line.strip().split(',')
+#         building = parts[0]
+#         cost = float(parts[1])
+#
+#         # building, cost = line.strip().split(',')
+#         # cost = float(cost)
+#
+#         print(f"{building} costs ${cost:.2f}")
 
-        parts = line.strip().split(',')
-        building = parts[0]
-        cost = float(parts[1])
+FILENAME = "number.txt"
 
-        # building, cost = line.strip().split(',')
-        # cost = float(cost)
 
-        print(f"{building} costs ${cost:.2f}")
+def main():
+    secret = load_number(FILENAME)
+    guess = get_valid_number()
+    while guess != secret:
+        print("Guess again!")
+        guess = get_valid_number()
+    print("You got it!")
+
+
+def get_valid_number():
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            guess = int(input("Guess: "))
+            is_valid_input = True
+        except ValueError:
+            print("Invalid integer")
+    return guess
+
+
+def load_number(filename):
+    try:
+        infile = open(filename, "r")
+        number = int(infile.read())
+    except ValueError:
+        print(f"Invalid contents in {filename}")
+        number = 6
+    except FileNotFoundError:
+        print(f"{filename} not found")
+        number = 4
+    else:
+        infile.close()
+    return number
+
+
+main()
