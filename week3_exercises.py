1e0cf08f78ace75191a7deeeb836108c51405437
David C. Page <davidpage876@gmail.com>
2022-09-29 12:01:05 +1000

    Indicate teacher example between # lines

--- week3_exercises.py
+++ week3_exercises.py
@@ -27,42 +27,49 @@
 #
 #         print(f"{building} costs ${cost:.2f}")
 
-FILENAME = "number.txt"
+
+######################################################
+# FILENAME = "number.txt"
+#
+#
+# def main():
+#     secret = load_number(FILENAME)
+#     guess = get_valid_number()
+#     while guess != secret:
+#         print("Guess again!")
+#         guess = get_valid_number()
+#     print("You got it!")
+#
+#
+# def get_valid_number():
+#     is_valid_input = False
+#     while not is_valid_input:
+#         try:
+#             guess = int(input("Guess: "))
+#             is_valid_input = True
+#         except ValueError:
+#             print("Invalid integer")
+#     return guess
+#
+#
+# def load_number(filename):
+#     try:
+#         infile = open(filename, "r")
+#         number = int(infile.read())
+#     except ValueError:
+#         print(f"Invalid contents in {filename}")
+#         number = 6
+#     except FileNotFoundError:
+#         print(f"{filename} not found")
+#         number = 4
+#     else:
+#         infile.close()
+#     return number
+#
+#
+# main()
+######################################################
 
 
-def main():
-    secret = load_number(FILENAME)
-    guess = get_valid_number()
-    while guess != secret:
-        print("Guess again!")
-        guess = get_valid_number()
-    print("You got it!")
 
 
-def get_valid_number():
-    is_valid_input = False
-    while not is_valid_input:
-        try:
-            guess = int(input("Guess: "))
-            is_valid_input = True
-        except ValueError:
-            print("Invalid integer")
-    return guess
-
-
-def load_number(filename):
-    try:
-        infile = open(filename, "r")
-        number = int(infile.read())
-    except ValueError:
-        print(f"Invalid contents in {filename}")
-        number = 6
-    except FileNotFoundError:
-        print(f"{filename} not found")
-        number = 4
-    else:
-        infile.close()
-    return number
-
-
-main()
