d9aac21ffe22f6fa0953b9b63df8e55cffbe3ec9
David C. Page <davidpage876@gmail.com>
2022-10-06 16:05:19 +1000

    Complete first "do-this-now" exercise

---
+++ week4_exercises.py
@@ -0,0 +1,20 @@
+"""Week 4 exercises"""
+
+# names = ["Dave", "Page", "Craig", "Me"]
+#
+# is_valid_choice = False
+# while not is_valid_choice:
+#     try:
+#         number = int(input(f"Select name to display (1-{len(names)}): "))
+#         # assert choice > 0
+#         if number <= 0:
+#             raise IndexError()
+#         print(names[number - 1])
+#         is_valid_choice = True
+#     except IndexError:
+#         print(f"Choice must be a valid number")
+#     # except AssertionError:
+#     #     print(f"Choice must be a valid number")
+
+
+
