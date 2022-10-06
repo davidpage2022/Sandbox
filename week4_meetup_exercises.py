cd88b9d91bd8c0cbfd525705aeee422320895328
David C. Page <davidpage876@gmail.com>
2022-10-06 13:08:25 +1000

    Write my exercise 4 in-class activity

---
+++ week4_meetup_exercises.py
@@ -0,0 +1,12 @@
+"""Week 4 exercises during meet up"""
+
+names = ["Ada", "Alan", "Bill", "John"]
+print(", ".join(names))
+name_to_remove = input("Who do you want to remove?")
+while name_to_remove != "":
+    try:
+        names.remove(name_to_remove)
+    except ValueError:
+        print(f"{name_to_remove} is not in the list")
+    print(", ".join(names))
+    name_to_remove = input("Who do you want to remove?")
