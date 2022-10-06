624b88d6a0762b8cafa936534dc8ce22f0a471a4
David C. Page <davidpage876@gmail.com>
2022-10-06 13:49:07 +1000

    Do more class exercises

--- week4_meetup_exercises.py
+++ week4_meetup_exercises.py
@@ -1,12 +1,24 @@
 """Week 4 exercises during meet up"""
 
-names = ["Ada", "Alan", "Bill", "John"]
-print(", ".join(names))
-name_to_remove = input("Who do you want to remove?")
-while name_to_remove != "":
-    try:
-        names.remove(name_to_remove)
-    except ValueError:
-        print(f"{name_to_remove} is not in the list")
-    print(", ".join(names))
-    name_to_remove = input("Who do you want to remove?")
+# names = ["Ada", "Alan", "Bill", "John"]
+# print(", ".join(names))
+# name_to_remove = input("Who do you want to remove?")
+# while name_to_remove != "":
+#     try:
+#         names.remove(name_to_remove)
+#     except ValueError:
+#         print(f"{name_to_remove} is not in the list")
+#     print(", ".join(names))
+#     name_to_remove = input("Who do you want to remove?")
+
+
+# Good
+# oo
+# T1G1
+# 20  ?
+#
+
+
+# 33
+
+
