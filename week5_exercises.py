00303398fe04d53577de9a4bbcdfe534907669e7
David C. Page <davidpage876@gmail.com>
2022-10-13 13:42:50 +1000

    Sort by score (descending)

--- week5_exercises.py
+++ week5_exercises.py
@@ -1,7 +1,9 @@
 """Week 5 exercises"""
 
+from operator import itemgetter
+
 data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelley', 9]]
 
 max_length = max(len(person) for person, score in data)
-for person, score in data:
+for person, score in sorted(data, key=itemgetter(1), reverse=True):
     print(f"{person:{max_length}} = {score:>3}")
