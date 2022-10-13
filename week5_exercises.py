cce35eadf85359da2ffe615ec05bd1cf46813184
David C. Page <davidpage876@gmail.com>
2022-10-13 14:17:17 +1000

    Convert exercise to use dictionary

--- week5_exercises.py
+++ week5_exercises.py
@@ -2,8 +2,16 @@
 
 from operator import itemgetter
 
-data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelley', 9]]
+# data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelley', 9]]
+name_to_number = {'Derek': 7, 'Xavier': 80, 'Bob': 612, 'Chantanelley': 9}
 
-max_length = max(len(person) for person, score in data)
-for person, score in sorted(data, key=itemgetter(1), reverse=True):
-    print(f"{person:{max_length}} = {score:>3}")
+# name_to_number = {person: score for person, score in data}
+# print(name_to_number)
+
+# max_length = max(len(person) for person, score in data)
+# for person, score in sorted(data, key=itemgetter(1), reverse=True):
+#     print(f"{person:{max_length}} = {score:>3}")
+
+max_length = max(len(name) for name in name_to_number.keys())
+for name, number in name_to_number.items():
+    print(f"{name:{max_length}} = {number:>3}")
