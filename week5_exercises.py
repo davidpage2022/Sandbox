09d6a911dc607a2349260880ef5d155e8fa68407
David C. Page <davidpage876@gmail.com>
2022-10-13 13:33:35 +1000

    Use list generator to make code less verbose

--- week5_exercises.py
+++ week5_exercises.py
@@ -2,9 +2,6 @@
 
 data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelley', 9]]
 
-max_length = 0
-for person, score in data:
-    if len(person) > max_length:
-        max_length = len(person)
+max_length = max(len(person) for person, score in data)
 for person, score in data:
     print(f"{person:{max_length}} = {score:>3}")
