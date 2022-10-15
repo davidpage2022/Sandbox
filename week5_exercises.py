9d77db7724cb28eeb58634fda018be750a1825a4
David C. Page <davidpage876@gmail.com>
2022-10-15 16:22:39 +1000

    Do week 5 warm-up exercise

--- week5_exercises.py
+++ week5_exercises.py
@@ -2,8 +2,9 @@
 
 from operator import itemgetter
 
+
 # data = [['Derek', 7], ['Xavier', 80], ['Bob', 612], ['Chantanelley', 9]]
-name_to_number = {'Derek': 7, 'Xavier': 80, 'Bob': 612, 'Chantanelley': 9}
+# name_to_number = {'Derek': 7, 'Xavier': 80, 'Bob': 612, 'Chantanelley': 9}
 
 # name_to_number = {person: score for person, score in data}
 # print(name_to_number)
@@ -12,6 +13,26 @@ name_to_number = {'Derek': 7, 'Xavier': 80, 'Bob': 612, 'Chantanelley': 9}
 # for person, score in sorted(data, key=itemgetter(1), reverse=True):
 #     print(f"{person:{max_length}} = {score:>3}")
 
-max_length = max(len(name) for name in name_to_number.keys())
-for name, number in name_to_number.items():
-    print(f"{name:{max_length}} = {number:>3}")
+# max_length = max(len(name) for name in name_to_number.keys())
+# for name, number in name_to_number.items():
+#     print(f"{name:{max_length}} = {number:>3}")
+
+
+def main():
+    names = ["Dave", "Susan", "Jane"]
+    ages = [45, 55, 35]
+
+    print(find_oldest_person(names, ages))
+
+
+def find_oldest_person(names, ages):
+    oldest_person = names[0]
+    oldest_age = ages[0]
+    for i in range(len(names)):
+        if ages[i] > oldest_age:
+            oldest_person = names[i]
+            oldest_age = ages[i]
+    return oldest_person
+
+
+main()
