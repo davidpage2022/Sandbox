7c97c5be4c4025f76950c895bf89be0da9efd1cd
David C. Page <davidpage876@gmail.com>
2022-10-24 10:12:06 +1000

    Do warm-up exercise for week 6

---
+++ week6_exercises.py
@@ -0,0 +1,8 @@
+"""Week 6 exercises"""
+
+ON_SALE_INDEX = 2
+
+products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
+on_sale_products = [product for product in products if product[ON_SALE_INDEX]]
+
+print(on_sale_products)
