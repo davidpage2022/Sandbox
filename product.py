a75c711a40a8b5d4c42cc83e5ed709570506d200
David C. Page <davidpage876@gmail.com>
2022-10-24 10:23:43 +1000

    Add product class

---
+++ product.py
@@ -0,0 +1,24 @@
+class Product:
+    def __init__(self, name="", price=0.0, is_on_sale=False):
+        self.name = name
+        self.price = price
+        self.is_on_sale = is_on_sale
+
+    def __str__(self):
+        if self.is_on_sale:
+            on_sale_string = "(on sale)"
+        else:
+            on_sale_string = ""
+        return f"{self.name}, ${self.price:.2f} {on_sale_string}"
+
+    def __repr__(self):
+        return str(self)
+
+
+# p = Product("Phone", 340, True)
+# print(p)
+
+products = [Product("Phone", 340, False), Product("PC", 1420.95, True), Product("Plant", 24.5, True)]
+
+on_sale_products = [product for product in products if product.is_on_sale]
+print(on_sale_products)
