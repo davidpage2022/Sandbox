5d9af06ebfa8c1bcfaedd392a4a487d33ae78a59
David C. Page <davidpage876@gmail.com>
2022-10-24 10:33:30 +1000

    Extend product class

--- product.py
+++ product.py
@@ -14,11 +14,22 @@ class Product:
     def __repr__(self):
         return str(self)
 
+    def put_on_sale(self, discount_percentage):
+        """Put Product on sale by discount_percentage as decimal (e.g., 0.2 is 20% off)."""
+        self.price *= (1 - discount_percentage)
+        self.is_on_sale = True
 
-# p = Product("Phone", 340, True)
-# print(p)
+
+p = Product("Phone", 340, False)
+print(p.name, "....")
+print(p)
+p.put_on_sale(0.1)
+print(p)
+
+print(type(p))
+
 
 products = [Product("Phone", 340, False), Product("PC", 1420.95, True), Product("Plant", 24.5, True)]
 
 on_sale_products = [product for product in products if product.is_on_sale]
-print(on_sale_products)
+# print(on_sale_products)
