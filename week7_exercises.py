235a27613bb04cfdfc175a1dace2b842e06cc66f
David C. Page <davidpage876@gmail.com>
2022-11-03 10:49:11 +1000

    Do equality operator overload exercise

--- week7_exercises.py
+++ week7_exercises.py
@@ -13,6 +13,15 @@ class City:
         return City(self.name, self.population + other.population)
 
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+
 def test_city():
     home = City("Home", 5000)
     away = City("Away", 10399)
@@ -21,5 +30,12 @@ def test_city():
     print(home + away)
 
 
+def test_person():
+    people = [Person("Bob", 35), Person("Jane", 29), Person("Bill", 29), Person("Bob", 35)]
+    print(people[0] == people[1])
+    print(people[1] == people[2])
+    print(people[3] == people[0])
+
+
 if __name__ == '__main__':
-    test_city()
+    test_person()
