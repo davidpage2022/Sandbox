8a00b918ccdaa89317a0734cdb7fdc3b3b7fe880
David C. Page <davidpage876@gmail.com>
2022-11-09 10:17:41 +1000

    Use .kv file to declare widget layout

--- week8_exercises.py
+++ week8_exercises.py
@@ -2,11 +2,13 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.lang import Builder
 
 
 class HelloWorld(App):
     def build(self):
-        self.root = Widget()
+        self.title = "Hello world!"
+        self.root = Builder.load_file('widget.kv')
         return self.root
 
 
