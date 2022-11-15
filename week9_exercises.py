c8e892716832f48254f4ba0c696e0f28f5674c62
David C. Page <davidpage876@gmail.com>
2022-11-15 18:50:16 +1000

    Use property instead

--- week9_exercises.py
+++ week9_exercises.py
@@ -14,6 +14,7 @@ class Person:
         date_string = datetime.datetime.strftime(self.date_of_birth, "%d/%m/%Y")
         return f"{self.name} ({date_string})"
 
+    @property
     def age(self) -> int:
         return int((datetime.date.today() - self.date_of_birth).days / DAYS_PER_YEAR)
 
@@ -21,7 +22,7 @@ class Person:
 def test_person():
     person = Person("John Smith", datetime.date(1992, 1, 1))
     print(person.name)
-    print(person.age())
+    print(person.age)
 
 
 if __name__ == '__main__':
