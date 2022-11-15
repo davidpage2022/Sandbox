70b81540432a94159b619002ce7ede0055d85a13
David C. Page <davidpage876@gmail.com>
2022-11-15 18:46:25 +1000

    Use noun-style naming convention for calculated attribute

--- week9_exercises.py
+++ week9_exercises.py
@@ -14,13 +14,14 @@ class Person:
         date_string = datetime.datetime.strftime(self.date_of_birth, "%d/%m/%Y")
         return f"{self.name} ({date_string})"
 
-    def get_age(self) -> int:
+    def age(self) -> int:
         return int((datetime.date.today() - self.date_of_birth).days / DAYS_PER_YEAR)
 
 
 def test_person():
     person = Person("John Smith", datetime.date(1992, 1, 1))
-    print(person.get_age())
+    print(person.name)
+    print(person.age())
 
 
 if __name__ == '__main__':
