4e86949e693c2f4ac012a41b9525e0434cd94528
David C. Page <davidpage876@gmail.com>
2022-11-16 19:12:42 +1000

    Call parent constructor from child

--- week9_exercises.py
+++ week9_exercises.py
@@ -19,11 +19,30 @@ class Person:
         return int((datetime.date.today() - self.date_of_birth).days / DAYS_PER_YEAR)
 
 
+class Student(Person):
+    def __init__(self, name, date_of_birth, student_id, course):
+        # Person.__init__(self, name, date_of_birth)
+        # super(Student, self).__init__(name, date_of_birth)
+        super().__init__(name, date_of_birth)
+        self.course = course
+        self.id = student_id
+
+    def __repr__(self):
+        return str(vars(self))
+
+
 def test_person():
     person = Person("John Smith", datetime.date(1992, 1, 1))
     print(person.name)
     print(person.age)
 
 
+def test_student():
+    student = Student("Jane Doe", datetime.date(1992, 1, 1), 3523676, "Information Technology")
+    print(student)
+    print(student.age)
+
+
 if __name__ == '__main__':
-    test_person()
+    # test_person()
+    test_student()
