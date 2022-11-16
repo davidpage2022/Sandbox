297774ca8912eceffbeafd0886aad7f9d02a027b
David C. Page <davidpage876@gmail.com>
2022-11-16 19:36:53 +1000

    Add employee and musician classes

--- week9_exercises.py
+++ week9_exercises.py
@@ -31,6 +31,27 @@ class Student(Person):
         return str(vars(self))
 
 
+class Employee(Person):
+    def __init__(self, salary, **kwargs):
+        super().__init__(**kwargs)
+        self.salary = salary
+
+    def __repr__(self):
+        return str(vars(self))
+
+
+class Musician(Person):
+    def __init__(self, style, **kwargs):
+        super().__init__(**kwargs)
+        self.style = style
+
+    def __repr__(self):
+        return str(vars(self))
+
+    def play(self, duration):
+        print(f"Playing for {duration} seconds in {self.style} style...")
+
+
 def test_person():
     person = Person("John Smith", datetime.date(1992, 1, 1))
     print(person.name)
@@ -44,6 +65,23 @@ def test_student():
     print(student.age)
 
 
+def test_employee():
+    employee = Employee(name="Bob", date_of_birth=datetime.date(1892, 1, 1),
+                        salary=500)
+    print(employee)
+    print(employee.salary)
+
+
+def test_musician():
+    musician = Musician(name="Sally", date_of_birth=datetime.date(1900, 2, 15),
+                        style="blues")
+    print(musician)
+    print(musician.style)
+    musician.play(5)
+
+
 if __name__ == '__main__':
     # test_person()
-    test_student()
+    # test_student()
+    # test_employee()
+    test_musician()
